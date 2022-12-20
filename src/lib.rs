b6b1fc5fb38f4605628dada6698d6cf03b7f5df0
Yushun Cheng <chengys@disroot.org>
2022-12-20 10:13:16 +0800

    Add Prefix, Postfix, Prepended, and Appended.

--- src/lib.rs
+++ src/lib.rs
@@ -74,3 +74,95 @@ impl<I> Ended for Index<I> {
 
 /// A struct with Span.
 pub type Span<T> = Start<End<T>>;
+
+/// With an optional prefix.
+pub struct Prefix<A, B> {
+    pub prefix: Option<A>,
+    pub inner: B,
+}
+
+impl<A: Started, B: Started> Started for Prefix<A, B> {
+    #[inline]
+    fn start(&self) -> usize {
+        self.prefix
+            .as_ref()
+            .map(|prefix| prefix.start())
+            .unwrap_or(self.inner.start())
+    }
+}
+
+impl<A, B: Ended> Ended for Prefix<A, B> {
+    #[inline]
+    fn end(&self) -> usize {
+        self.inner.end()
+    }
+}
+
+/// With an optional postfix.
+pub struct Postfix<A, B> {
+    pub inner: A,
+    pub postfix: Option<B>,
+}
+
+impl<A: Started, B> Started for Postfix<A, B> {
+    #[inline]
+    fn start(&self) -> usize {
+        self.inner.start()
+    }
+}
+
+impl<A: Ended, B: Ended> Ended for Postfix<A, B> {
+    #[inline]
+    fn end(&self) -> usize {
+        self.postfix
+            .as_ref()
+            .map(|postfix| postfix.end())
+            .unwrap_or(self.inner.end())
+    }
+}
+
+/// `AAA...B`
+pub struct Prepended<A, B> {
+    pub precedings: Vec<A>,
+    pub last: B,
+}
+
+impl<A: Started, B: Started> Started for Prepended<A, B> {
+    #[inline]
+    fn start(&self) -> usize {
+        self.precedings
+            .first()
+            .map(|first| first.start())
+            .unwrap_or(self.last.start())
+    }
+}
+
+impl<A, B: Ended> Ended for Prepended<A, B> {
+    #[inline]
+    fn end(&self) -> usize {
+        self.last.end()
+    }
+}
+
+/// `ABBB...`
+pub struct Appended<A, B> {
+    pub first: A,
+    pub followings: Vec<B>,
+}
+
+impl<A: Started, B> Started for Appended<A, B> {
+    #[inline]
+    fn start(&self) -> usize {
+        self.first.start()
+    }
+}
+
+impl<A: Ended, B: Ended> Ended for Appended<A, B> {
+    #[inline]
+    fn end(&self) -> usize {
+        self.followings
+            .last()
+            .map(|item| item.end())
+            .unwrap_or(self.first.end())
+    }
+}
